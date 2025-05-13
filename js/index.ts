2846b92bb12e5ad0bcb0931b608d7e8309b5ce7a
김재승 <jaes2878@gmail.com>
2025-05-13 12:40:31 +0900

    모든 json 요청 완료

--- js/index.ts
+++ js/index.ts
@@ -1,7 +1,27 @@
-import apiFunc from "./api/api.js";
+import apiFunc from "./api/apits.js";
 // console.log(apiFunc);
 
 //* 경력
 apiFunc("career").then((data) => {
-  console.log(data);
+  console.log("경력", data);
+});
+
+apiFunc("careerInfo").then((data) => {
+  console.log("경력기술서", data);
+});
+
+apiFunc("certificate").then((data) => {
+  console.log("자격증", data);
+});
+
+apiFunc("introduce").then((data) => {
+  console.log("자소서", data);
+});
+
+apiFunc("portfolio").then((data) => {
+  console.log("포트폴리오", data);
+});
+
+apiFunc("training").then((data) => {
+  console.log("경력이수", data);
 });
